fb7614eff9f39f49165423d4f59a7edbe4d21d01
wwixull <daranosaceva74@gmail.com>
2026-04-21 21:01:38 +0700

    auth and reg

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, url_for, render_template, session
+import database 
 
 app = Flask(__name__)
 app.secret_key = "oidjwoiqj./uqj0q9/871d23hqd45f6"
@@ -7,5 +8,61 @@ app.secret_key = "oidjwoiqj./uqj0q9/871d23hqd45f6"
 def index():
     return render_template('index.html')
 
+@app.route("/register", methods=["POST", "GET"])
+def register_page():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        login = request.form["login"]
+        pass1 = request.form["pass1"]
+        pass2 = request.form["pass2"]
+        errors = []
+
+
+        if database.check_user_exists(login):
+            errors.append("Такой пользователь уже существует")
+
+        if pass1 != pass2:
+            errors.append("Пароли не совпадают")
+        
+
+        if len(pass1) < 8:
+            errors.append("Длина пароля должна быть больше 8 символов")
+        
+
+        if len(errors) == 0:
+            database.add_user(login, pass1)
+            return render_template("success_register.html")
+        else:
+            return render_template("register.html", errors=errors)
+        
+@app.route("/login", methods=["POST", "GET"])
+def login_page():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        login = request.form["login"]
+        password = request.form["password"]
+        user_id = database.auth_user(login, password)
+
+        if user_id >= 0:
+            print("Успешный вход")
+            session["user_id"] = user_id
+            session["login"] = login
+            return redirect(url_for("main_page"))
+        else:
+            print("Что-то не так")
+            return render_template("login.html", errors=["Неверный логин или пароль"])
+
+@app.route("/admin")
+def admin_page():
+    users = database.get_users()
+
+    return render_template("admin.html", users=users)
+
+@app.route('/main_page')
+def main_page():
+    return render_template('main_page.html')
+        
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import sqlite3
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
+SALT = "kdfjkjwekjq002302mdmclcs[ck,]"
 
 def create_db():
     conn = sqlite3.connect("books.db")
@@ -38,3 +40,45 @@ def create_db():
             # 0 - план
             # 1 - в процессе
             # 2 - прочитано
+
+def add_user(login, password):
+    conn = sqlite3.connect("books.db")
+    cursor = conn.cursor()
+    hashed_password = generate_password_hash(password + SALT)
+
+    cursor.execute("INSERT INTO user (login, password) VALUES (?,?)", (login, hashed_password))
+
+    conn.commit()
+
+def get_users():
+    conn = sqlite3.connect("books.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM user")
+    users = cursor.fetchall()
+    return users
+
+def check_user_exists(login):
+    conn = sqlite3.connect("books.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM user WHERE login=?", (login,))
+
+    user = cursor.fetchone()
+    return True if user else False
+
+def auth_user(login, password):
+
+    conn = sqlite3.connect("books.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user WHERE login = ?", (login,))
+    user = cursor.fetchone()
+    if not user:
+        return -1
+    
+    if check_password_hash(user[2], password+SALT):
+        return user[0]
+    return -1
+
+if __name__ == "__main__":
+    create_db()
